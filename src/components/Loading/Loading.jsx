3b4bd3ee4a64a5b301e2a2184645e866023c48e7
Luis Ramirez <lerc7689@hotmail.com>
2023-05-09 19:06:30 -0400

    Proof

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,7 +3,7 @@ import "../Loading/Loading.css";
 const Loading = () => {
   return (
     <div className="Loading">
-      <img src="./src/assets/icons/loading.gif" alt="" />
+      <img src="./src/assets/icons/load.gif" alt="" />
     </div>
   );
 };
